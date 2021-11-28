3aaa5b2ec970f8a6d7c7e84c4064b628497e94c4
Richard Blewett <richardcblewett@gmail.com>
2021-11-28 14:09:40 -0500

    moved the players inside of the gameboard class in order to make htings more manageable

--- js/script.js
+++ js/script.js
@@ -6,20 +6,24 @@ class Gameboard {
     createNewGameboard = () => {   //a new gameboord class comes with a grid, but a new game needs a clean one
         grid = [];
         for (let i = 0; i < 9; i++) { gameboard.push(''); } //blank-fill the array 
+        players = [(new Player('X')),(new Player('O'))];
+        turn = 0;
     }
-    players = [];
+    players = [(new Player('X')),(new Player('O'))];
+    turn = 0;
 }
-class Player {
+class Player { //instances of this class should be part of the gameboaord class. 
     constructor(name) { this.name = name.toString(); };
     moves = [];
     won = false;
     recordMove = (move) => {    // takes a numerical play and adds it to a list of player moves 
         this.moves.push(move);
+        console.log('move recorded' + move);
         if (this.moves.length >= 3) {
             this.winningConditions();
         }
-        if (won === true) {
-            console.log(this.name);//somehting happens;
+        if (this.won === true) {
+            console.log(this.name + 'WINNING');//something happens;
         };
     };
     winningConditions = () => { //input will be the moves by the player
@@ -58,16 +62,17 @@ class Player {
 }
 
 const ttt = new Gameboard;
-const X = new Player('X');
-const O = new Player('O');
-ttt.players.push('X');
-ttt.players.push('O');
+//const X = new Player('X');
+//const O = new Player('O');
+//ttt.players.push(new Player('X'));
+//ttt.players.push(new Player('O'));
 let turn = 0;
 
 document.querySelectorAll(".square").forEach(elem => {
     elem.addEventListener('click', () => {                  //if there's no value in the area
         if (elem.textContent.length === 0) {
-            elem.textContent = ttt.players[turn % 2];
+            elem.textContent = ttt.players[turn % 2].name;
+            ttt.players[turn % 2].recordMove(parseInt(elem.id));
             turn++;
         }
     })
